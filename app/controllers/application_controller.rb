5d2ec9a8f82537525d80c4d62fddc07a88ce9d52
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-29 19:00:46 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < Sinatra::Base
   get '/' do 
     
   end
+
+
+  get '/posts/new' do 
+    erb :new
+  end
+
+  get '/posts' do 
+    @posts = Post.all
+    erb :index
+  end
+
+  post '/posts' do 
+    @post = Post.create(params)
+    redirect to '/posts'
+    # get /posts should display all posts through index.erb
+  end
+
+  get '/posts/:id' do 
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do 
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do 
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    erb :show
+    #do not use redirect w/ :id
+  end
+
+  delete '/posts/:id/delete' do 
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :delete
+  end
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+  # @@all = []
+
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   @@all << self
+  # end
+
+  # def self.all 
+  #   @@all 
+  # end
+end
